ce2e0a2ca6a404a982eee67c311163177c898e45
momo226-code <nanaidriss70@gmail.com>
2025-02-25 01:54:25 +0000

    Update App.vue

--- TP1/src/App.vue
+++ TP1/src/App.vue
@@ -1,47 +1,80 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <p>Exercice1</p>
+  <h1> {{ titre }}</h1>
+  <button @click="clique()"> "Cliquez moi !!" </button>
+<p>Exercice2</p>
+  <div>
+    <button @click="incrementer()">+ +</button> {{ compteur }}  <button @click="decrementer()">- -</button>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+
+
+
+<p>Exercice3</p>
+<input type="text" v-model="texte" @input="verifier">
+<p v-if="space">Vous aviez mis un espace !</p>
+
+<p>Exercice4</p>
+
+
+<ul>
+  <li>Couleur de la police :</li>
+  
+      <div><label for="">Rouge</label><input type="radio" v-model="couleur" value="red" id="red"></div>
+      
+      <div><label for="">Bleu</label><input type="radio" v-model="couleur" value=blue id="blue"></div>
+  <li>Afficher/cacher le paragraphe? <input type="checkbox" v-model="para"></li>
+</ul>
+
+<p  v-bind:style="{color:couleur}" v-if="para">
+  Dieu merci pour tout ce qu'il a fait pour moi jusque maintenant
+  je sais que la vie d'ici bas est un enfer pour le croyant et un paradis pour le mecrean
+</p>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+
+export default {
+  data(){
+    return{
+      titre : "Cliquez moi !!" ,
+      compteur : 0,
+      texte:'',
+      space :false ,
+     couleur:'' ,
+      para:false 
+    } ;
+  },
+  methods:{
+    verifier(){
+      if(this.texte.includes(' ')){
+        this.space=true ;
+      }else{
+        this.space=false ;
+      }
+    } , 
+
+    clique(){
+      this.titre = "Cliqué!" ; 
+    } ,
+
+    incrementer(){
+      this.compteur ++ ;
+    } ,
+
+    decrementer(){
+      this.compteur--; 
+    }
+
+  }
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+<style>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
