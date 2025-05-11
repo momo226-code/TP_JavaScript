87ebb609635f79d532794568e1bf381b41886f31
Idriss <nanaidriss70@gmail.com>
2025-05-12 00:06:05 +0100

    Add files via upload

---
+++ Spotify_clone/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <Header/>
+    <NuxtPage/>
+   
+  </div>
+</template>
---
+++ Spotify_clone/components/header.vue
@@ -0,0 +1,9 @@
+<template>
+
+    <nav>
+
+        <div> <NuxtLink to="/">Acceuil</NuxtLink>  </div>
+
+        <NuxtLink to="/blog">Blog</NuxtLink>
+    </nav>
+</template>
